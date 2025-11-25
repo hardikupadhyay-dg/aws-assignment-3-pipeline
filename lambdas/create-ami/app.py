e17a84f9bcb4c1fd1c21339fb1c31808e5a404a0
hardikupadhyay-dg <hardik.upadhyay@datagrokr.co>
2025-11-25 10:52:25 +0530

    new changes

--- lambdas/create-ami/app.py
+++ lambdas/create-ami/app.py
@@ -34,50 +34,29 @@ import os
 ec2 = boto3.client('ec2', region_name=os.environ.get("AWS_REGION"))
 
 def lambda_handler(event, context):
-    instance_id = event.get("existing-instance-id") or event.get("InstanceId") or event.get("instance_id")
+    instance_id = (
+        event.get("existing-instance-id")
+        or event.get("InstanceId")
+        or event.get("instance_id")
+    )
     if not instance_id:
-        raise Exception("existing-instance-id required in the event")
-
-    # 1) make sure the instance exists (otherwise CreateImage will fail)
-    try:
-        resp = ec2.describe_instances(InstanceIds=[instance_id])
-    except ec2.exceptions.ClientError as e:
-        raise
-
-    reservations = resp.get("Reservations", [])
-    if not reservations or not reservations[0].get("Instances"):
-        raise Exception(f"Instance {instance_id} not found")
+        raise Exception("existing-instance-id required")
 
     name = f"ami-from-{instance_id}-{int(time.time())}"
-    try:
-        resp = ec2.create_image(InstanceId=instance_id, Name=name, NoReboot=True)
-    except Exception as e:
-        # bubble up with clear message
-        raise
+    resp = ec2.create_image(
+        InstanceId=instance_id,
+        Name=name,
+        NoReboot=True
+    )
 
-    image_id = resp["ImageId"]
+    image_id = resp.get("ImageId")
+    if not image_id:
+        raise Exception("Failed to create AMI")
 
-    # Wait for image to become 'available'
-    waiter = ec2.get_waiter('image_available')
+    # Wait until AMI becomes available
+    waiter = ec2.get_waiter("image_available")
     waiter.wait(ImageIds=[image_id])
 
-    # Refresh image data to find snapshot IDs
-    desc = ec2.describe_images(ImageIds=[image_id])
-    ami = desc["Images"][0]
-
-    snapshot_ids = []
-    for bd in ami.get("BlockDeviceMappings", []):
-        ebs = bd.get("Ebs")
-        if ebs and ebs.get("SnapshotId"):
-            snapshot_ids.append(ebs["SnapshotId"])
-
-    # Wait for each snapshot to be completed (if any)
-    if snapshot_ids:
-        snap_waiter = ec2.get_waiter('snapshot_completed')
-        snap_waiter.wait(SnapshotIds=snapshot_ids)
-
-    # Return ami_id for downstream steps
     return {
-        **event,
-        "ami_id": image_id
+        "ImageId": image_id
     }
--- lambdas/launch-instance/app.py
+++ lambdas/launch-instance/app.py
@@ -63,34 +63,58 @@
 
 
 
-
 import boto3
 import os
 
 ec2 = boto3.client('ec2')
 
+KEY_NAME = os.environ.get("KEY_NAME", "")
+DEFAULT_INSTANCE_TYPE = os.environ.get("DEFAULT_INSTANCE_TYPE", "t2.micro")
+
 def lambda_handler(event, context):
+    image_id = event.get("ImageId")
+    existing_instance_id = event.get("existing-instance-id")
 
-    image_id = event.get('ami_id')
-    if not image_id:
-        raise Exception("ami_id required")
+    if not image_id or not existing_instance_id:
+        raise Exception("ImageId and existing-instance-id required")
 
-    instance_type = "t2.micro"
+    # Describe existing instance
+    desc = ec2.describe_instances(InstanceIds=[existing_instance_id])
+    reservations = desc.get("Reservations", [])
+    if not reservations:
+        raise Exception("Existing instance not found")
 
-    response = ec2.run_instances(
-        ImageId=image_id,
-        InstanceType=instance_type,
-        MinCount=1,
-        MaxCount=1,
-        KeyName=os.environ.get("KEY_NAME"),
-        NetworkInterfaces=[{
-            "AssociatePublicIpAddress": True,
-            "DeviceIndex": 0,
-            "SubnetId": os.environ.get("SUBNET_ID"),
-            "Groups": [os.environ.get("SG_ID")]
-        }]
-    )
+    ins = reservations[0]["Instances"][0]
+
+    instance_type = ins.get("InstanceType") or DEFAULT_INSTANCE_TYPE
+    subnet_id = ins.get("SubnetId")
+    sg_ids = [sg["GroupId"] for sg in ins.get("SecurityGroups", [])] or None
+    key_name = ins.get("KeyName") or (KEY_NAME if KEY_NAME else None)
+
+    params = {
+        "ImageId": image_id,
+        "InstanceType": instance_type,
+        "MinCount": 1,
+        "MaxCount": 1
+    }
+    if subnet_id:
+        params["SubnetId"] = subnet_id
+    if sg_ids:
+        params["SecurityGroupIds"] = sg_ids
+    if key_name:
+        params["KeyName"] = key_name
+
+    resp = ec2.run_instances(**params)
+    new_id = resp["Instances"][0]["InstanceId"]
+
+    try:
+        ec2.create_tags(
+            Resources=[new_id],
+            Tags=[{"Key": "CreatedBy", "Value": "StepFunctionAmiFlow"}]
+        )
+    except:
+        pass
 
     return {
-        "InstanceId": response["Instances"][0]["InstanceId"]
+        "new-instance-id": new_id
     }
